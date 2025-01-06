3a0c5371995e1ce0c731dc830791cc6a10c7b896
Icode-arch <Maxc723@hotmail.com>
2025-01-06 06:32:19 -0500

    2: Add Dictionary coutning each characther

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def main():
 	book_path ="books/frankenstein.txt"
 	text = get_book_text(book_path)
 	word_counts = number_of_words_in_book(text)
+	print(characthers)
 
 def get_book_text(path):
 	with open(path) as f:
@@ -13,11 +14,16 @@ def number_of_words_in_book(text):
 	words_so_far = 0
 	separate = text.split()
 	for word in separate:
-		characthers = get_number_of_each_characther(word)
+		characther = get_number_of_each_characther(word)
 		words_so_far += 1
 	return words_so_far
 
-def get_number_of_each_characther(Letter)
-	if Letter
+def get_number_of_each_characther(Letter):
+	lower_case_only = Letter.lower()
+	for char in lower_case_only:
+		if char in characthers:
+			characthers[char] += 1
+		else:		
+			characthers[char] = 1
 
 main()
